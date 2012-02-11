abebcbe1db888967db3575264baecd4cf8886a2d
BBakerpkt <bbakerpkt@gmail.com>
2012-02-10 21:34:51 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RpsApp::Application.config.secret_token = '9f1311ce60beac9774f6ce91bb1cb1d20bd1050c2afff43a0c3523ca7178266a65af6b0eab0fe60d61b5eb6d112561a94e42975cd0850a4ce834731a510cdf50'
